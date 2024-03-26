9af0f3f7aedfe4664aeee9616bbd6c44e08fa339
leite santana <Rodrigo_l.santana20@hotmail.com>
2024-03-25 21:44:55 -0300

    Cadastro de Profissionais

--- Aula-04-Eclipse-Depuracao/src/meuPrimeiroProjeto/Principal.java
+++ Aula-04-Eclipse-Depuracao/src/meuPrimeiroProjeto/Principal.java
@@ -7,33 +7,62 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		
-		// Inicializando os Objetos 
+		// 			Inicializando os Objetos 
 		
-		AdmBanco admBanco = new AdmBanco();
+		       AdmBanco admBanco = new AdmBanco();
+		       Programador programador = new Programador();
 		
-		//'''''''''''Recebendo dados''''''''''''''''''''''''''''''''''''''''
+		//					Recebendo dados
 		
-		Scanner entradaDados = new Scanner(System.in);
-		
-		
-		//'''''''''''Iniciando comunicacao com o Console'''''''''''''''''''''
+		       Scanner entradaDados = new Scanner(System.in);
 		
 		
-		System.out.println("Cadastro do Profissional");
+		//			Iniciando comunicacao com o Console
 		
-		//'''''''''' Se Comunicando com o Usuario pelo console'''''''''''''''
+		
+		       System.out.println("Cadastro do Profissional");
+				
+		
+		//					Cadastro do AdmBanco'''''''
+		
+		       System.out.println("Qual é o seu nome:    ");
+		       admBanco.Nome = entradaDados.next();
+		
+		       System.out.println(admBanco.Nome +    " qual é o seu CPF:    ");
+		       admBanco.Cpf = entradaDados.next();
+		
+
+		       System.out.println("Tipo de banco de dados:   ");
+		       admBanco.TipoDeBancoDeDados = entradaDados.next();
+		
+		
+		//					Cadastro do Programador
+		
+		
 		
 		System.out.println("Qual é o seu nome:    ");
-		admBanco.Nome = entradaDados.next();
+		programador.Nome = entradaDados.next();
 		
-		System.out.println("Qual é o seu CPF:    ");
-		admBanco.Cpf = entradaDados.next();
+		System.out.println(programador.Nome + " qual é o seu CPF:  ");
+		programador.Cpf = entradaDados.next();
 		
-		//'''''''''''Exibindo no Console o resultado''''''''''''''''''''''''''
+		System.out.println(" Tipo de Linguagem:   ");
+		programador.Linguagem  = entradaDados.next();
+		
+		
+		//				Exibindo no Console o resultado
+		
+		
 		
 		System.out.println(admBanco.Nome);
-
 		System.out.println(admBanco.Cpf);
+		System.out.println(admBanco.TipoDeBancoDeDados);
+		
+		
+		
+		System.out.println(programador.Nome);
+		System.out.println(programador.Cpf);
+		System.out.println(programador.Linguagem);
 		
 		
 	}
